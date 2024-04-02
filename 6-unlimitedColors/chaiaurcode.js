cb8a3437c5cd254270f93ad2e611063da7c1f4f7
gopalsharma456 <gopalsharma456@gmail.com>
2024-04-02 15:05:22 +0530

    Unlimited Bg color

---
+++ 6-unlimitedColors/chaiaurcode.js
@@ -0,0 +1,32 @@
+let randomColor = function () {
+    let hex = '0123456789ABCDEF';
+    let color = '#';
+  
+    for (let i = 0; i < 6; i++) {
+      color += hex[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
+  
+  let intervalId;
+  
+  const startBgColorBtn = function () {
+    function bgColor() {
+      document.body.style.backgroundColor = randomColor();
+    }
+  
+    if (!intervalId) {
+      intervalId = setInterval(bgColor, 1000);
+      console.log(randomColor());
+    }
+  };
+  
+  const stopBgColorBtn = function () {
+    clearInterval(intervalId);
+    intervalId = null;
+    // console.log(intervalId);
+  };
+  document.getElementById('start').addEventListener('click', startBgColorBtn);
+  
+  document.getElementById('start').addEventListener('click', stopBgColorBtn);
+  
