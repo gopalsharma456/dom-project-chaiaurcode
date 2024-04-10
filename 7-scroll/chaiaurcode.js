3a856a62337e920a428e4630f3fe79743cde788f
gopalsharma456 <gopalsharma456@gmail.com>
2024-04-10 15:03:58 +0530

    scroll bar added

--- 7-scroll/chaiaurcode.js
+++ 7-scroll/chaiaurcode.js
@@ -1,1 +1,13 @@
 // use indicator width
+window.onscroll = function () {
+  myFunction();
+};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById('myBar').style.width = scrolled + '%';
+}
