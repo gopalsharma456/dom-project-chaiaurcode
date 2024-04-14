7f1cb7464f2f4acf6749e26ee32c0831b82d8f78
null <null>
2024-04-14 12:31:10 +0530

    typing animation

--- 8-typer/chaiaurcode.js
+++ 8-typer/chaiaurcode.js
@@ -3,6 +3,39 @@ const cursor = document.querySelector('.cursor');
 
 const words = ['Love', 'Jhakaas', 'mast', 'dhinchak', 'Weird'];
 
-function type() {}
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
 
-function erase() {}
+function type() {
+  const word = words[wordIndex];
+
+  if (!isDeleting) {
+    typedTextSpan.textContent += word[charIndex];
+    charIndex++;
+    if (charIndex === word.length) {
+      isDeleting = true;
+      setTimeout(type, 1000);
+    } else {
+      setTimeout(type, 100);
+    }
+  } else {
+    erase();
+  }
+}
+
+function erase() {
+  const word = words[wordIndex];
+
+  typedTextSpan.textContent = word.substring(0, charIndex - 1);
+  charIndex--;
+  if (charIndex === 0) {
+    isDeleting = false;
+    wordIndex = (wordIndex + 1) % word.length;
+    setTimeout(type, 500);
+  } else {
+    setTimeout(erase, 100);
+  }
+}
+
+type();
